9e0179fa4a566e58c85f70b204f27989655fb9d6
Arnav <arnchkv@gmail.com>
2025-04-28 17:36:33 +0530

    Implement posts resource with CRUD functionality, including controller, views, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_28_113606) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_28_120616) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
