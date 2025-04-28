362ce5c4545b515a865cdcae4e81f47d04aa2b86
Arnav <arnchkv@gmail.com>
2025-04-28 17:26:44 +0530

    Implement sessions functionality with controller actions, views, routes, and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "sign_out", to: "sessions#destroy"
   get "registrations/new"
   get "registrations/create"
   get "about", to: "about#index"
